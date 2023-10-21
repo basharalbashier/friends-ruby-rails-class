f55efb039866c4842fff17f48c606615991eff4e
basharalbashier <basharalbashier@gmail.com>
2023-10-22 01:33:26 +0300

    adding crud with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  resources :friends
   get 'home/about'
   root  'home#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
